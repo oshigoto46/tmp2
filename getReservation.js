0c66cc6140e3d66b0cb76bff553fbb6e1cb8eb84
oshigoto46 <ppp.shiro46@gmail.com>
2021-09-02 20:00:33 +0300

    work ref

--- getReservation.js
+++ getReservation.js
@@ -1,20 +1,60 @@
-async function getReservation(call, callback) {
-  //if(true){console.log(call)}
-  let ret = await this.dataStore
-    .select(
-      `select * from reservation where reservationId=${call.request.reservationId}`
-    )
-    .then((val) => {
-      if (val.length === 1) {
-        return callback(null, response.200);
-      } else if (val.length === 0) {
-        return callback(null,response.404 );
-      } else {
-        return callback(null,response.409);
-      }})
-    .catch((val) => {
-      return callback(null,response.503 );
+async function makeReservation(call, callback) {
+  //console.log(call.request)
+
+  if (call.request.reservationSlot <= 1 || call.request.reservationSlot >= 10) {
+    return callback(null, {
+      responseCode: 400,
+      status: grpc.status.INTERNAL,
     });
+  }
+
+  await this.dataStore
+    .insert({
+      reservationId: call.request.reservationId,
+      reservationDate: call.request.reservationDate,
+      doctorId: call.request.doctorId,
+      clientId: call.request.clientId,
+      reservationSlot: call.request.reservationSlot,
+    })
+    .then((val) => {
+      if (val === 1) {
+        callback(null, {
+          responseCode: 201,
+          status: grpc.status.INTERNAL,
+        });
+      } else {
+        callback(null, {
+          responseCode: 500,
+          status: grpc.status.INTERNAL,
+        });
+      }
+    })
+    .catch(
+      (err) => {
+        let str = "" + err;
+        // console.log(err)
+        if (str.indexOf("ER_DUP_ENTRY") > 0) {
+          // console.log("重複エラー")
+          callback(null, {
+            responseCode: 409,
+            status: grpc.status.INTERNAL,
+          });
+        } else if (str.indexOf("ER_NO_REFERENCED_ROW_2") > 0) {
+          // console.log("重複エラー")
+          callback(null, {
+            responseCode: 400,
+            status: grpc.status.INTERNAL,
+          });
+        } else {
+          console.log("some another error" + err);
+          callback(null, {
+            responseCode: 102,
+            status: grpc.status.INTERNAL,
+          });
+        }
+      }
+      //results =>callback(null, { success: results})
+    );
 }
 
-module.exports = getReservation;
+module.exports = makeReservation;
